f3185f6dd69b183e58a465e888d1a6e4dd5c4ecf
fantasive <hhermeswang@gmail.com>
2016-09-19 19:37:09 +0800

    write a require by myself

---
+++ Require/fiveRequire.js
@@ -0,0 +1,21 @@
+/*实现一个require的基本结构
+ * time:2016.09.19
+ *email: hhermeswang@gmail.com
+ * author: eddie
+ * description: a requirejs
+ */
+(function(global) {
+    //定义两个加载的对象
+    //loadings:表示正在加载的模块
+    //modules: 表示加载的模块
+    var loadings = [],
+        modules = {};
+
+    //requirejs object
+    var fiveRequireJS = {};
+
+    //书写define函数
+    fiveRequireJS.define = function(deps, callback) {
+
+    };
+})(window);
