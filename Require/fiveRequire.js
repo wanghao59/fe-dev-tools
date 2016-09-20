7566d1abf936c06109ccc03966d34f2637a6e10d
fantasive <hhermeswang@gmail.com>
2016-09-20 10:07:25 +0800

    ..

--- Require/fiveRequire.js
+++ Require/fiveRequire.js
@@ -16,6 +16,31 @@
 
     //书写define函数
     fiveRequireJS.define = function(deps, callback) {
+        //得到当前define的路径
+        var id = fiveRequireJS.getCurrentJs();
+        //将依赖放入一个队列中
+        var depsId = [];
+        deps.map(function(name) {
+            depsId.push(fiveRequireJS.getScriptId(name));
+        });
+        //如果没有注册，就将其注册进入modules中
+        if( !modules[id] ) {
+            modules[id] = {
+                id: id,
+                state: 1,
+                deps: depsId,
+                callback: callback,
+                exports: null,
+                color: 0
+            }
+        }
+    };
 
+    fiveRequireJS.require = function(deps, callback) {
+
+    };
+
+    fiveRequireJS.getCurrentJs = function() {
+        return
     };
 })(window);
